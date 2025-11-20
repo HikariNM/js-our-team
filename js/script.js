bdb3e525185d07d4079aaa96a6df957d6818fc5f
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-20 21:59:27 +0100

    aggiunto form a comparsa per l'aggiunta card

--- js/script.js
+++ js/script.js
@@ -45,11 +45,11 @@ const teamMembers = [
 
 // for (let i = 0; i < teamMembers.length; i++) {
 //   const teamMember = teamMembers[i];
-  
+
 //   const {name, role, email, img} = teamMember;
 
 //   cardMember += generateCard(name, role, email, img)
-  
+
 // }
 
 // card.innerHTML = cardMember;
@@ -69,41 +69,106 @@ const teamMembers = [
 
 // }
 
-/******************VERSIONE MIGLIORATA CON ADD MEMBER***************** */
+/******************FUNCTION + ADD BUTTON CON PROMPT***************** */
 
-generateCard();
+// generateCard();
 
-function generateCard(){
-  cardHtml = document.getElementById('cardContainer');
-  let cardsList = '';
-  
-  for (let i = 0; i < teamMembers.length; i++) {
-    const teamMember = teamMembers[i];
+// function generateCard(){
+//   cardHtml = document.getElementById('cardContainer');
+//   let cardsList = '';
 
-    const card = `<div class="col-lg-4">
-    <div id="card" class="bg-black d-flex">
-    <img id="cardImg" src="${teamMember.img}" alt="${teamMember.name}">
-    <div class="card-body p-3">
-    <h4 id="name">${teamMember.name}</h4>
-    <p id="role">${teamMember.role}</p>
-    <a id="email" class="text-decoration-none" href="mailto:${teamMember.email}">${teamMember.email}</a>
-    </div>
-    </div>
-    </div>`;
-    
-     cardsList += card;
-    
-  }
-  cardHtml.innerHTML = cardsList;
+//   for (let i = 0; i < teamMembers.length; i++) {
+//     const teamMember = teamMembers[i];
 
-};
+//     const card = `<div class="col-lg-4">
+//     <div id="card" class="bg-black d-flex">
+//     <img id="cardImg" src="${teamMember.img}" alt="${teamMember.name}">
+//     <div class="card-body p-3">
+//     <h4 id="name">${teamMember.name}</h4>
+//     <p id="role">${teamMember.role}</p>
+//     <a id="email" class="text-decoration-none" href="mailto:${teamMember.email}">${teamMember.email}</a>
+//     </div>
+//     </div>
+//     </div>`;
+
+//      cardsList += card;
+
+//   }
+
+//   cardHtml.innerHTML = cardsList;
+
+// };
+
+// document.getElementById('addBtn').addEventListener('click', function(){
+//   teamMembers.push({
+//     name: prompt('Inserisci il nome'),
+//     role: prompt('Inserisci il ruolo'),
+//     email: prompt('Inserisci un indirizzo email valido'),
+//     img: prompt('Inserisci il URL di un immagine')
+//   });
+//   generateCard()
+// })
+
+/******************FUNCTION + ADD BUTTON CON FORM***************** */
+
+cardHtml = document.getElementById('cardContainer')
+
+renderCard();
 
 document.getElementById('addBtn').addEventListener('click', function(){
-  teamMembers.push({
-    name: prompt('Inserisci il nome'),
-    role: prompt('Inserisci il ruolo'),
-    email: prompt('Inserisci un indirizzo email valido'),
-    img: prompt('Inserisci il URL di un immagine')
+  event.preventDefault();
+  document.getElementById('cardFormRow').classList.replace('d-none', 'd-flex');
+  document.getElementById('addBtn').classList.add('d-none');
+});
+
+document.getElementById('submitBtn').addEventListener('click', function(){
+  event.preventDefault();
+  inputName = document.getElementById('inputName').value;
+  inputRole = document.getElementById('inputRole').value;
+  inputEmail = document.getElementById('inputEmail').value;
+  inputImg = document.getElementById('inputImg').value;
+  
+  const check = teamMembers.find(function(member){
+    return member.name === inputName;
+    });
+    
+    if (!check) {
+      teamMembers.push({
+        name: inputName,
+        role: inputRole,
+        email: inputEmail,
+        img: inputImg,
+      });
+      
+    } else alert('Card già inserita')
+    renderCard()
+
+    document.getElementById('cardFormRow').classList.replace('d-flex', 'd-none');
+    document.getElementById('addBtn').classList.replace('d-none', 'd-inline-block');
   });
-  generateCard()
-})
+  
+  
+  console.log(teamMembers)
+  
+  function renderCard() {
+    let cardList = '';
+    for (let i = 0; i < teamMembers.length; i++) {
+      const teamMember = teamMembers[i];
+  
+      const card = `<div class="col-lg-4">
+                        <div id="card" class="bg-black d-flex">
+                            <img id="cardImg" src="${teamMember.img}" alt="${teamMember.name}">
+                            <div class="card-body p-3">
+                                <h4 id="name">${teamMember.name}</h4>
+                                <p id="role">${teamMember.role}</p>
+                                <a id="email" class="text-decoration-none" href="mailto:${teamMember.email}">${teamMember.email}</a>
+                            </div>
+                          </div>
+                      </div>`;
+  
+      cardList += card
+  
+  
+    }
+    cardHtml.innerHTML = cardList;
+  };
