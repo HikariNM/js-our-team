8ca3122b8b6e910647c0881f029982be756296ef
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-21 22:21:33 +0100

    realizzata funzione che aggiunge il for in pagina

--- js/script.js
+++ js/script.js
@@ -109,29 +109,116 @@ const teamMembers = [
 //   generateCard()
 // })
 
-/******************FUNCTION + ADD BUTTON CON FORM***************** */
+/******************FUNCTION + ADD BUTTON CON FORM A CAMBIO VISIBILITA'***************** */
+
+// cardHtml = document.getElementById('cardContainer')
+
+// renderCard();
+
+// document.getElementById('addBtn').addEventListener('click', function(){
+//   event.preventDefault();
+//   document.getElementById('cardFormRow').classList.replace('d-none', 'd-flex');
+//   document.getElementById('addBtn').classList.add('d-none');
+// });
+
+// document.getElementById('submitBtn').addEventListener('click', function(){
+//   event.preventDefault();
+//   inputName = document.getElementById('inputName').value;
+//   inputRole = document.getElementById('inputRole').value;
+//   inputEmail = document.getElementById('inputEmail').value;
+//   inputImg = document.getElementById('inputImg').value;
+  
+//   const check = teamMembers.find(function(member){
+//     return member.name === inputName;
+//     });
+    
+//     if (!check) {
+//       teamMembers.push({
+//         name: inputName,
+//         role: inputRole,
+//         email: inputEmail,
+//         img: inputImg,
+//       });
+      
+//     } else alert('Card già inserita')
+//     renderCard()
+
+//     document.getElementById('cardFormRow').classList.replace('d-flex', 'd-none');
+//     document.getElementById('addBtn').classList.replace('d-none', 'd-inline-block');
+//   });
+  
+  
+//   console.log(teamMembers)
+  
+//   function renderCard() {
+//     let cardList = '';
+//     for (let i = 0; i < teamMembers.length; i++) {
+//       const teamMember = teamMembers[i];
+  
+//       const card = `<div class="col-lg-4">
+//                         <div id="card" class="bg-black d-flex">
+//                             <img id="cardImg" src="${teamMember.img}" alt="${teamMember.name}">
+//                             <div class="card-body p-3">
+//                                 <h4 id="name">${teamMember.name}</h4>
+//                                 <p id="role">${teamMember.role}</p>
+//                                 <a id="email" class="text-decoration-none" href="mailto:${teamMember.email}">${teamMember.email}</a>
+//                             </div>
+//                           </div>
+//                       </div>`;
+  
+//       cardList += card
+  
+  
+//     }
+//     cardHtml.innerHTML = cardList;
+//   };
+
+/******************FUNCTION CON FORM AD AGGIUNTA***************** */
 
 cardHtml = document.getElementById('cardContainer')
+form = document.getElementById('cardFormRow');
+let count = 0;
 
 renderCard();
 
-document.getElementById('addBtn').addEventListener('click', function(){
+document.getElementById('addBtn').addEventListener('click', function () {
   event.preventDefault();
-  document.getElementById('cardFormRow').classList.replace('d-none', 'd-flex');
-  document.getElementById('addBtn').classList.add('d-none');
-});
+  count++;
 
-document.getElementById('submitBtn').addEventListener('click', function(){
-  event.preventDefault();
-  inputName = document.getElementById('inputName').value;
-  inputRole = document.getElementById('inputRole').value;
-  inputEmail = document.getElementById('inputEmail').value;
-  inputImg = document.getElementById('inputImg').value;
-  
-  const check = teamMembers.find(function(member){
-    return member.name === inputName;
+  if (count % 2 != 0) {
+    form.innerHTML = `<div class="col-3">
+    <form id="CardForm" action="">
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="inputName" placeholder="Nome Cognome">
+        </div>
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Ruolo</label>
+            <input type="text" class="form-control" id="inputRole" placeholder="Ruolo">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Indirizzo Email</label>
+            <input type="email" class="form-control" id="inputEmail" placeholder="Indirizzo Email" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">URL Immagine</label>
+            <input type="text" class="form-control" id="inputImg" placeholder="Inserisci un URL per l'immagine">
+        </div>
+      
+    </form>
+  </div>`
+
+  }
+  else {
+    const inputName = document.getElementById('inputName').value;
+    const inputRole = document.getElementById('inputRole').value;
+    const inputEmail = document.getElementById('inputEmail').value;
+    const inputImg = document.getElementById('inputImg').value;
+
+    const check = teamMembers.find(function (member) {
+      return member.name === inputName;
     });
-    
+
     if (!check) {
       teamMembers.push({
         name: inputName,
@@ -139,23 +226,28 @@ document.getElementById('submitBtn').addEventListener('click', function(){
         email: inputEmail,
         img: inputImg,
       });
-      
-    } else alert('Card già inserita')
-    renderCard()
 
-    document.getElementById('cardFormRow').classList.replace('d-flex', 'd-none');
-    document.getElementById('addBtn').classList.replace('d-none', 'd-inline-block');
-  });
-  
-  
-  console.log(teamMembers)
-  
-  function renderCard() {
-    let cardList = '';
-    for (let i = 0; i < teamMembers.length; i++) {
-      const teamMember = teamMembers[i];
-  
-      const card = `<div class="col-lg-4">
+      form.innerHTML = ``;
+
+    } else {
+      alert('Card già inserita');
+      count--;
+    }
+    
+    renderCard();
+
+  }
+
+});
+
+console.log(teamMembers)
+
+function renderCard() {
+  let cardList = '';
+  for (let i = 0; i < teamMembers.length; i++) {
+    const teamMember = teamMembers[i];
+
+    const card = `<div class="col-lg-4">
                         <div id="card" class="bg-black d-flex">
                             <img id="cardImg" src="${teamMember.img}" alt="${teamMember.name}">
                             <div class="card-body p-3">
@@ -165,10 +257,10 @@ document.getElementById('submitBtn').addEventListener('click', function(){
                             </div>
                           </div>
                       </div>`;
-  
-      cardList += card
-  
-  
-    }
-    cardHtml.innerHTML = cardList;
-  };
+
+    cardList += card
+
+
+  }
+  cardHtml.innerHTML = cardList;
+};
